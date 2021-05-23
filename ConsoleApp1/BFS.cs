7835df5777c003bc6d8834c1b47f06663ba38e3c
Shankar S <Shankaraswamy.S@Resideo.com>
2021-05-23 21:06:31 +0530

    BFS - added a BFS class

---
+++ ConsoleApp1/BFS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// will demonstrate applications of BFS like
+    ///  - To find the level of each node
+    ///  - 0-1 BFS to find out shortest distance between source and any given node in the tree.
+    /// </summary>
+    internal class BFS
+    {
+
+    }
+}
